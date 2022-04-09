1683bca14120ba993be99cf5ece3d341ec3051b8
Tấn phúc Trần phan <tanphucb01@gmail.com>
2022-04-09 16:55:59 +0700

    Login_Api.sql

---
+++ Login_Api.sql
@@ -0,0 +1,13 @@
+﻿--đây là database nháp được tạo ra để test api Login_Api.
+
+CREATE DATABASE Tuyensinh;
+
+CREATE TABLE DangNhap
+(
+	LoginId INT UNIQUE NOT NULL ,
+	UserName NVARCHAR(36) NOT NULL ,
+	Matkhau NVARCHAR(36) NOT NULL,
+)
+
+ALTER TABLE DangNhap
+ADD CONSTRAINT PK_DangNhap PRIMARY KEY (LoginId);
